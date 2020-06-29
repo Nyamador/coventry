52051ae5711c60f3561bbd80a27aeaabeb4a743e
Nyamador <dselasi4u@gmail.com>
2020-06-29 00:12:31 +0000

    Sending

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const amount = document.querySelector('.amount');
 const overlay = document.querySelector('.overlay');
 const sendBtn = document.querySelector('#send-btn');
 
-const loader = `<svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="40px" height="40px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve">
+const loader = `<svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="20px" height="40px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve">
 <path fill="#ffffff" d="M25.251,6.461c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615V6.461z" transform="rotate(303.774 25 25)">
   <animateTransform attributeType="xml" attributeName="transform" type="rotate" from="0 25 25" to="360 25 25" dur="0.6s" repeatCount="indefinite"></animateTransform>
   </path>
@@ -22,8 +22,17 @@ function toggleForm(){
 }
 
 function mimicSending(){
-        sendBtn.innerHtml = loader;
+    
+    setTimeout(function (){
+            sendBtn.style.color = 'green';
+            sendBtn.innerHtml = 'Success';
+            window.location.reload();
+    }, 2000)
+    sendBtn.style.padding = '4px';
+    sendBtn.innerHTML = loader;
 }
+
+
 
 sendBtn.addEventListener('click', mimicSending);
 
